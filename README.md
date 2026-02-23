60e4313f19676038a38cebde26bb8681a75a37df
Jay Miles <jomiles26@googlemail.com>
2026-02-23 14:57:42 +0000

    first working draft

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# eggd_vcf_normaliser
-Normalise variants in a VCF using bcftools norm
+# eggd_vcf_normaliser (DNAnexus platform app)
+
+## What does this app do?
+
+Normalises variants in a supplied input VCF, using bcftools norm via the htslib suite of tools (v1.22.0).
+
+<br></br>
+
+## What are typical use cases for this app?
+
+
+<br></br>
+
+## What inputs are required for this app to run?
+
+input_vcf: A VCF file of variants to normalise
+fasta_tar: A tar archive of a reference genome fasta and its index
+
+<br></br>
+
+## What does this app output?
+
+output_vcf: Product of normalising the input VCF using bcftools norm
+output_index: Index file for output VCF
+
+<br></br>
+
+This is the source code for an app that runs on the DNAnexus Platform. For more information about how to run or modify it, see https://documentation.dnanexus.com/.
+
+#### This app was made by EMEE GLH
---
+++ src/code.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+_download_inputs() {
+    mark-section "Downloading inputs"
+    echo "Downloading input files..."
+    dx-download-all-inputs --parallel
+    # Move files to the correct directory
+    find ~/in/ -type f -name "*" -print0 | xargs -0 -I {} mv {} /home/dnanexus
+}
+
+_extract_reference() {
+    mark-section "Extracting reference"
+    echo "Extracting reference archive..."
+    tar -I pigz -xf "$fasta_tar_name"
+    genome_fasta="genome.fa"
+}
+
+_create_output_name() {
+    mark-section "Creating output filenames"
+    echo "Creating output filenames..."
+    output_vcf="${input_vcf_prefix}_normalised.vcf.gz"
+    output_index="${input_vcf_prefix}_normalised.vcf.gz.tbi"
+}
+
+_normalise_vcf() {
+    mark-section "Normalising and indexing VCF"
+    echo "Normalising and indexing VCF..."
+    bcftools norm "$input_vcf_name" -f "$genome_fasta" -m -any --keep-sum AD -Oz -o "$output_vcf"
+    tabix -p vcf "$output_vcf"
+}
+
+_upload_outputs() {
+    mark-section "Uploading outputs"
+    echo "Uploading outputs..."
+    uploaded_vcf=$(dx upload "$output_vcf" --brief)
+    uploaded_index=$(dx upload "$output_index" --brief)
+    dx-jobutil-add-output output_vcf "$uploaded_vcf" --class=file
+    dx-jobutil-add-output output_index "$uploaded_index" --class=file
+}
+
+
+main() {
+    set -exo pipefail
+    _download_inputs
+    _extract_reference
+    _create_output_name
+    _normalise_vcf
+    _upload_outputs
+    echo "Done!"
+    mark-success
+}
